f5181d1781e8192287caff10af42e807f806d475
George Knyazyan <jorj.knyazyan.15@gmail.com>
2025-07-16 00:09:18 +0300

    Перемещать виртуальные линии диагностики от pyright

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,3 +12,17 @@ map("n", "<leader>fg", "<cmd> Telescope live_grep<cr>")
 map("n", "<leader>fb", "<cmd> Telescope buffers<cr>")
 map("n", "<leader>fh", "<cmd> Telescope help_tags<cr>")
 
+
+map("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+
+map('n', '<leader>k', function()
+  vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+
+  vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('line-diagnostics', { clear = true }),
+    callback = function()
+      vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+      return true
+    end,
+  })
+end)
